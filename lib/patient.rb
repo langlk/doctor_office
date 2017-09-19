91d055f869698aa1b66cec6874cfaa96b7c7cf93
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-19 09:12:35 -0700

    Add initialize and accessors for Patient object

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,9 +1,19 @@
 #!/usr/bin/env ruby
 
 class Patient
-  attr_reader :id, :name, :birthday, :doctor_id
+  attr_reader :id, :name, :birthday
+  attr_accessor :doctor_id
 
   def initialize(attributes)
-
+    @id = nil
+    @doctor_id = nil
+    if attributes.has_key?(:id)
+      @id = attributes[:id]
+    end
+    if attributes.has_key?(:doctor_id)
+      @doctor_id = attributes[:doctor_id]
+    end
+    @name = attributes[:name]
+    @birthday = attributes[:birthday]
   end
 end
--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -3,4 +3,35 @@
 require "spec-helper"
 
 describe('Patient') do
+  it "saves the name, id, birthday, and doctor_id of a patient" do
+    attributes = {:name => "Frank", :birthday => "1990-01-01", :doctor_id => 1, :id => 5}
+    patient_test = Patient.new(attributes)
+    expect(patient_test.name).to(eq("Frank"))
+    expect(patient_test.id).to(eq(5))
+    expect(patient_test.doctor_id).to(eq(1))
+    expect(patient_test.birthday).to(eq("1990-01-01"))
+  end
+
+  describe('#id') do
+    it "starts out as nil" do
+      attributes = {:name => "Frank", :birthday => "1990-01-01"}
+      patient_test = Patient.new(attributes)
+      expect(patient_test.id).to(eq(nil))
+    end
+  end
+
+  describe('#doctor_id') do
+    it "starts out as nil" do
+      attributes = {:name => "Frank", :birthday => "1990-01-01"}
+      patient_test = Patient.new(attributes)
+      expect(patient_test.doctor_id).to(eq(nil))
+    end
+
+    it "can be set by user" do
+      attributes = {:name => "Frank", :birthday => "1990-01-01"}
+      patient_test = Patient.new(attributes)
+      patient_test.doctor_id = 1
+      expect(patient_test.doctor_id).to(eq(1))
+    end
+  end
 end

8c571047a73e08290105d6539d47ff7bd0dc23c2
Kelsey Langlois <kels.langlois@gmail.com>
2017-09-20 18:02:35 -0700

    Refactor Patient to clean up code.

--- lib/patient.rb
+++ lib/patient.rb
@@ -5,24 +5,17 @@ class Patient
   attr_accessor :doctor_id, :name, :birthday
 
   def initialize(attributes)
-    @id = nil
-    @doctor_id = -1
-    if attributes.has_key?(:id)
-      @id = attributes[:id]
-    end
-    if attributes.has_key?(:doctor_id)
-      @doctor_id = attributes[:doctor_id]
-    end
+    @id = attributes[:id] || nil
+    @doctor_id = attributes[:doctor_id] || -1
     @name = attributes[:name]
     @birthday = attributes[:birthday]
   end
 
   def ==(other_patient)
-    ids_equal = @id == other_patient.id
-    names_equal = @name == other_patient.name
-    birthdays_equal = @birthday == other_patient.birthday
-    doctors_equal = @doctor_id == other_patient.doctor_id
-    ids_equal & names_equal & birthdays_equal & doctors_equal
+    (@id == other_patient.id) &
+    (@name == other_patient.name) &
+    (@birthday == other_patient.birthday) &
+    (@doctor_id == other_patient.doctor_id)
   end
 
   def save
@@ -39,28 +32,26 @@ class Patient
   end
 
   def self.all
-    all_patients = []
     results = DB.exec("SELECT * FROM patients;")
-    results.each do |result|
-      attributes = {}
-      attributes[:id] = result["id"].to_i
-      attributes[:name] = result["name"]
-      attributes[:birthday] = result["birthday"][0...10]
-      attributes[:doctor_id] = result["doctor_id"].to_i
-      all_patients.push(Patient.new(attributes))
+    results.map do |result|
+      Patient.new({
+        id: result["id"].to_i,
+        name: result["name"],
+        birthday: result["birthday"][0...10],
+        doctor_id: result["doctor_id"].to_i
+      })
     end
-    all_patients
   end
 
   def self.find(patient_id)
     results = DB.exec("SELECT * FROM patients WHERE id = #{patient_id};")
     if results.any?
-      attributes = {}
-      attributes[:id] = results.first["id"].to_i
-      attributes[:name] = results.first["name"]
-      attributes[:birthday] = results.first["birthday"][0...10]
-      attributes[:doctor_id] = results.first["doctor_id"].to_i
-      return Patient.new(attributes)
+      return Patient.new({
+        id: results.first["id"].to_i,
+        name: results.first["name"],
+        birthday: results.first["birthday"][0...10],
+        doctor_id: results.first["doctor_id"].to_i
+      })
     else
       return nil
     end
